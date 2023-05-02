cd4ab4557160390f96db423d808b81db118ae99a
nate <nathanz@nycstudents.net>
2023-05-02 09:32:47 -0400

    update

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,11 +1,10 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.ActionListener;
 
-public class mainPanel extends JFrame implements ActionListener, KeyListener {
+public class MainClass extends JFrame implements ActionListener, KeyListener {
     private JPanel TipCalculator;
     private JLabel BillLabel;
     private JTextField answerBL;
@@ -21,19 +20,73 @@ public class mainPanel extends JFrame implements ActionListener, KeyListener {
     private JLabel totalTipLabel;
     private JLabel totalLabel;
     private JTextField resultTotal;
+    private JButton calculateButton;
 
-    public mainPanel() {
+    private TipCalculator tipC;
+    private double numOfPeople;
+    private double tipPercentage;
+    private double bill;
+
+    public MainClass() {
         createUIComponents();
     }
 
-    private void createUIComponents() {
-        setContentPane(mainPanel);
+    public void createUIComponents() {
+        setContentPane(TipCalculator);
         setTitle("My GUI!");
+        setSize(600, 400);
+        setLocation(450,100);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        subtractTip.addActionListener(this);
+        addTip.addActionListener(this);
+        subtractPpl.addActionListener(this);
+        addPpl.addActionListener(this);
+        setVisible(true);
     }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source instanceof JButton) {
+            JButton button = (JButton) source;
+            String text = button.getText();
+            if (button.equals(addTip)) {
+                double value = Double.parseDouble(answerTipL.getText());
+                value+=0.5;
+                answerTipL.setText(value + "");
+                tipPercentage = Double.parseDouble(answerTipL.getText());
 
+            }
+            if (button.equals(subtractTip)) {
+                double value = Double.parseDouble(answerTipL.getText());
+                if (value >= 0.5) {
+                    value -= 0.5;
+                    answerTipL.setText(value + "");
+                }
+                tipPercentage = Double.parseDouble(answerTipL.getText());
+            }
+            if (button.equals(addPpl)) {
+                double value = Double.parseDouble(answerNP.getText());
+                value+=0.5;
+                answerNP.setText(value + "");
+                numOfPeople = Double.parseDouble(answerNP.getText());
+            }
+            if (button.equals(subtractPpl)) {
+                double value = Double.parseDouble(answerNP.getText());
+                if (value >= 0.5) {
+                    value -= 0.5;
+                    answerNP.setText(value + "");
+                }
+                numOfPeople = Double.parseDouble(answerNP.getText());
+            }
+            if (button.equals(calculateButton)) {
+                bill = Double.parseDouble(answerBL.getText());
+                tipC = new TipCalculator(bill, tipPercentage, numOfPeople);
+                resultTip.setText(tipC.calculateTip() + "");
+                resultTotal.setText(tipC.totalBill() + "");
+            }
+        }
     }
 
     @Override
--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class TipCalculator {
+public class TipCalculator
+{
+
+    //Attributes should be bill, tip percentage, number of people.
+    private double bill;
+    private double tipPercentage;
+    private double numberOfPeople;
+
+    public TipCalculator(double bill, double tipPercentage, double numberOfPeople)
+    {
+        this.bill = bill;
+        this.tipPercentage = tipPercentage;
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    //getters and setters for all three attributes
+    public double getBill() {return bill;}
+    public double getTip() {return tipPercentage;}
+    public double getNumberOfPeople() {return numberOfPeople;}
+    public void setBill(double newBill) {bill = newBill;}
+    public void setTipPercentage(int newPercent) {tipPercentage = newPercent;}
+    public void setNumberOfPeople(int newNumberOfPeople) {numberOfPeople = newNumberOfPeople;}
+
+    //calculate the tip
+    public double calculateTip()
+    {
+        return bill*tipPercentage/100.0;
+    }
+
+    //calculate total bill
+    public double totalBill()
+    {
+        return bill + calculateTip();
+    }
+
+    //calculate tip per person
+    public double tipPerPerson()
+    {
+        return calculateTip()/numberOfPeople;
+    }
+
+    //Total per person
+    public double totalPerPerson()
+    {
+        return bill/numberOfPeople + tipPerPerson();
+    }
 }
